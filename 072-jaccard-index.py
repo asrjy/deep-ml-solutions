11eb98cbb455838482b1cd940a0d2bd425367a44
Yashwanth <yashwanth.alakky@gmail.com>
2025-04-11 21:53:06 +0530

    072: jaccard index

---
+++ 072-jaccard-index.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def jaccard_index(y_true, y_pred):
+    intersection = np.logical_and(y_true, y_pred).sum()
+    union = np.logical_or(y_true, y_pred).sum()
+    if union == 0:
+        return 1.0  
+    return round(intersection / union, 3)
