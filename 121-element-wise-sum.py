84cff3c0f6625605067514f5bdeddf4bc4009d1b
Yashwanth <yashwanth.alakky@gmail.com>
2025-04-19 23:07:59 +0530

    121: element wise sum

---
+++ 121-element-wise-sum.py
@@ -0,0 +1,5 @@
+def vector_sum(a: list[int|float], b: list[int|float]) -> list[int|float]:
+    if len(a) != len(b):
+        return -1 
+    else:
+        return [a[i] + b[i] for i in range(len(a))]
